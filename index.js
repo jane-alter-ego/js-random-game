7bb32a491f9c9908702544199e4228a5d75a8ad4
jane alter ego <zh.antipushina@gmail.com>
2023-10-14 18:30:42 -0300

    feat: add gradient background

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let canvas;
-
+let raf;
 
 document.addEventListener("DOMContentLoaded", () => {
     canvas = document.getElementById("canvas");
@@ -12,4 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
         canvas.width = header.getBoundingClientRect().width;
         canvas.height = window.innerHeight - header.getBoundingClientRect().height - footer.getBoundingClientRect().height;
     })
+
+    window.requestAnimationFrame(draw);
 })
+
+const draw = () => {
+    drawRadialGradient();
+}
+
+const drawRadialGradient = () => {
+    const ctx = canvas.getContext('2d');
+    const gradient = ctx.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width * 1.2);
+    gradient.addColorStop(0, 'lightblue');
+    gradient.addColorStop(1, 'magenta');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
